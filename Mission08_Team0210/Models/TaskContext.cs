1f8d7f2af3456d8cd9d5dac207357a23219c0244
petjken <petjken@byu.edu>
2026-02-23 10:08:27 -0700

    ok i did my models

---
+++ Mission08_Team0210/Models/TaskContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mission08_TeamXXXX.Models;
+
+namespace Mission08_Team0210.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TaskModel> Tasks { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        // This part populates the Categories table as required by the instructions
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = "Home" },
+                new Category { CategoryId = 2, CategoryName = "School" },
+                new Category { CategoryId = 3, CategoryName = "Work" },
+                new Category { CategoryId = 4, CategoryName = "Church" }
+            );
+        }
+    }
+}
